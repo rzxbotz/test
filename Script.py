6da49b982a125018770d0f4f1a9979b7db470dca
rzxbotz <rzxbots@gmail.com>
2025-02-25 00:54:00 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,98 +1,94 @@
+
 class script(object):
-  START_MSG = """<b>Here You Can Find MG University's Study Notes & Previous Year Question Papers Easily.
-  
-  Select Your Course Using The Button Given Below:</b>"""
+    START_MSG = """<b>Here You Can Find MG University's Study Notes & Previous Year Question Papers Easily.
+    
+    Select Your Course Using The Button Given Below:</b>"""
 
-  COURSE = """<b>Select Your Course Using the Buttons Given Below</b>"""
-  
-  BCOM = """<i>○ Selected Course: BCOM</i>"""
+    COURSE = """<b>Select Your Course Using the Buttons Given Below</b>"""
 
-  MATERIALS2019 = """<i>○ <i>Selected Course:</i> <b>B.COM</b>
-  
-  ○ Here, You can Access Study Notes & Previous Year Question Papers! Click the Button Below to Continue:</i>"""
-  
-  BCM2019SNSEMESTERS = """<i>○ Selected Course: B.COM
-  
-  ○ NoW Select The Semester to Access the Study Materials!</i>"""
-  
-  BCMSNSEM1 = """<i>Selected Semester: First
+    BCOM = """<i>○ Selected Course: BCOM</i>"""
 
-Files Will Be Adding Soon...
+    MATERIALS2019 = """<i>○ Selected Course: <b>B.COM</b>
+    
+    ○ Here, You can Access Study Notes & Previous Year Question Papers! Click the Button Below to Continue:</i>"""
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMSNSEM2 = """<i>Selected Semester: Second
+    BCM2019SNSEMESTERS = """<i>○ Selected Course: B.COM
+    
+    ○ Now Select The Semester to Access the Study Materials!</i>"""
 
-Files Will Be Adding Soon...
+    BCMSNSEM1 = """<i>○ Selected Semester: First
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMSNSEM3 = """<i>Selected Semester: Third
+    Files Will Be Added Soon...
 
-Files Will Be Adding Soon...
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMSNSEM4 = """<i>Selected Semester: Fourth
+    BCMSNSEM2 = """<i>○ Selected Semester: Second
 
-Files Will Be Adding Soon...
+    Files Will Be Added Soon...
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMSNSEM5 = """<i>Selected Semester: Fifth
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-Files Will Be Adding Soon...
+    BCMSNSEM3 = """<i>○ Selected Semester: Third
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMSNSEM6 = """<i>Selected Semester: Sixth
+    Files Will Be Added Soon...
 
-Files Will Be Adding Soon...
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCM2019PYQSEMESTERS =  """<i>○ Selected Course: B.COM
-  
-  ○ NoW Select The Semester to Access the Previous Year Question Papes!</i>"""
-  
-  BCMPYQSEM1 = """<i>Selected Semester: First
+    BCMSNSEM4 = """<i>○ Selected Semester: Fourth
 
-Files Will Be Adding Soon...
+    Files Will Be Added Soon...
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMPYQSEM2 = """<i>Selected Semester: Second
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-Files Will Be Adding Soon...
+    BCMSNSEM5 = """<i>○ Selected Semester: Fifth
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMPYQSEM3 = """<i>Selected Semester: Third
+    Files Will Be Added Soon...
 
-Files Will Be Adding Soon...
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMPYQSEM4 = """<i>Selected Semester: Fourth
+    BCMSNSEM6 = """<i>○ Selected Semester: Sixth
 
-Files Will Be Adding Soon...
+    Files Will Be Added Soon...
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMPYQSEM5 = """<i>Selected Semester: Fifth
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-Files Will Be Adding Soon...
+    BCM2019PYQSEMESTERS = """<i>○ Selected Course: B.COM
+    
+    ○ Now Select The Semester to Access the Previous Year Question Papers!</i>"""
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
-  
-  BCMPYQSEM6 = """<i>Selected Semester: Sixth
+    BCMPYQSEM1 = """<i>○ Selected Semester: First
 
-Files Will Be Adding Soon...
+    Files Will Be Added Soon...
 
-NB: Click on the Subject Name to Get Study Materials .</i>"""
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
 
-  
+    BCMPYQSEM2 = """<i>○ Selected Semester: Second
 
+    Files Will Be Added Soon...
 
-  
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
+
+    BCMPYQSEM3 = """<i>○ Selected Semester: Third
+
+    Files Will Be Added Soon...
+
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
+
+    BCMPYQSEM4 = """<i>○ Selected Semester: Fourth
+
+    Files Will Be Added Soon...
+
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
+
+    BCMPYQSEM5 = """<i>○ Selected Semester: Fifth
+
+    Files Will Be Added Soon...
+
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
+
+    BCMPYQSEM6 = """<i>○ Selected Semester: Sixth
+
+    Files Will Be Added Soon...
+
+    NB: Click on the Subject Name to Get Study Materials.</i>"""
   
