6d564118341ffcf8791daad6dc2f2dab12e95c6c
rzxbotz <rzxbots@gmail.com>
2025-02-26 01:52:40 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -3,19 +3,18 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from Script import script
 from config import START_PIC
 
-@Client.on_message(filters.command("about") & filters.incoming)
+@Client.on_message(filters.command("about"))
 async def about(client, message):
-    buttons = [[
-        InlineKeyboardButton("Learn More About Us!", callback_data="abouts")
-    ],[
-        InlineKeyboardButton("Credit", callback_data="credit")
-    ],[
-        InlineKeyboardButton("𝖢𝗅𝗈𝗌𝖾", callback_data="close_data")
-    ]]
+    buttons = [
+        [InlineKeyboardButton("Learn More About Us", callback_data="abouts")],
+        [InlineKeyboardButton("Credits", callback_data="credit")],
+        [InlineKeyboardButton("Close", callback_data="close_data")]
+    ]
+    
     reply_markup = InlineKeyboardMarkup(buttons)
 
     await message.reply_photo(
-        photo=START_PIC,  # Now using START_PIC from script.py
+        photo=START_PIC,  
         caption=script.ABOUT_TXT,
         reply_markup=reply_markup
     )
