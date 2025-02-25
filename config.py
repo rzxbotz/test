fa8ba70e3aa242d8733269774fbc5cdda0a8d89e
rzxbotz <rzxbots@gmail.com>
2025-02-25 09:48:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,6 +36,7 @@ JOIN_REQUEST_ENABLE = os.environ.get("JOIN_REQUEST_ENABLED", None)
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
+START_PIC = os.environ.get("START_PIC","https://graph.org/file/91c3fd3ae19e2a2686088.jpg")
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
